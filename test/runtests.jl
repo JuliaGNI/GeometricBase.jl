89cca8d3a8d03f5ce0ca40d28dbbe4d49ff67c76
Michael Kraus <michael.kraus@ipp.mpg.de>
2021-08-20 16:06:19 +0200

    Add tests for Solution interface.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,3 +2,4 @@ using SafeTestsets
 
 @safetestset "Utils                                                                           " begin include("test_utils.jl") end
 @safetestset "Time Series                                                                     " begin include("timeseries_tests.jl") end
+@safetestset "Solution                                                                        " begin include("solution_tests.jl") end
