c480702766189ac5854649286e398c5df195a0be
Michael Kraus <michael.kraus@ipp.mpg.de>
2023-08-03 07:01:02 +0200

    Fix docstrings of NullParameters, NullInvariants and NullPeriodicity.

--- src/types.jl
+++ src/types.jl
@@ -23,22 +23,22 @@ export vectorfield, zerovector
 `NullInvariants` is an empty struct that is used to indicate than an equation does not have invariants.
 """
 struct NullInvariants end
-Base.getindex(::NullInvariants, i...) = error("Invariants were indexed but the invariants are `nothing`. You likely forgot to pass invariants to the Equation!")
-Base.iterate(::NullInvariants) = error("Invariants were indexed but the invariants are `nothing`. You likely forgot to pass invariants to the Equation!")
+Base.getindex(::NullInvariants, i...) = error("Invariants were indexed but the invariants are `NullInvariants`. You likely forgot to pass invariants to the Equation!")
+Base.iterate(::NullInvariants) = error("Invariants were indexed but the invariants are `NullInvariants`. You likely forgot to pass invariants to the Equation!")
 
 """
 `NullParameters` is an empty struct that is used to indicate than an equation does not have parameters.
 """
 struct NullParameters end
-Base.getindex(::NullParameters, i...) = error("Parameters were indexed but the parameters are `nothing`. You likely forgot to pass parameters to the Equation!")
-Base.iterate(::NullParameters) = error("Parameters were indexed but the parameters are `nothing`. You likely forgot to pass parameters to the Equation!")
+Base.getindex(::NullParameters, i...) = error("Parameters were indexed but the parameters are `NullParameters`. You likely forgot to pass parameters to the Equation!")
+Base.iterate(::NullParameters) = error("Parameters were indexed but the parameters are `NullParameters`. You likely forgot to pass parameters to the Equation!")
 
 """
 `NullPeriodicity` is an empty struct that is used to indicate than an equation does not have periodic solution components.
 """
 struct NullPeriodicity end
-Base.getindex(::NullPeriodicity, i...) = error("Periodicity was indexed but the periodicity is `nothing`. You likely forgot to pass periodicity to the Equation!")
-Base.iterate(::NullPeriodicity) = error("Periodicity was indexed but the periodicity is `nothing`. You likely forgot to pass periodicity to the Equation!")
+Base.getindex(::NullPeriodicity, i...) = error("Periodicity was indexed but the periodicity is `NullPeriodicity`. You likely forgot to pass periodicity to the Equation!")
+Base.iterate(::NullPeriodicity) = error("Periodicity was indexed but the periodicity is `NullPeriodicity`. You likely forgot to pass periodicity to the Equation!")
 
 
 const OptionalArray{arrayType} = Union{Nothing, arrayType} where {arrayType <: AbstractArray}
