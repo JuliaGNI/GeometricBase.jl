7aea9b9ba4ff00ebe0f0421268f2f4392c374767
Michael Kraus <michael.kraus@ipp.mpg.de>
2026-02-02 09:40:11 +0100

    Add NullSolverState type.

--- src/abstract_solver_state.jl
+++ src/abstract_solver_state.jl
@@ -12,8 +12,15 @@ isAbstractSolverState(::Any) = false
 isAbstractSolverState(::AbstractSolverState) = true
 
 """
-    SolverState
-
-This method returns a subtype of `AbstractSolverState` according to the `SolverMethod` and additional, required parameters that is passed to it.
+`NullSolverState` is an empty struct that is used to indicate than a solver does not store a state.
 """
-function SolverState end
+struct NullSolverState <: AbstractSolverState end
+
+
+"""
+    SolverState(::Union{<:AbstractSolver,<:SolverMethod}, args...; kwargs...)
+
+This method returns a subtype of `AbstractSolverState` according to the `AbstractSolver` or `SolverMethod` that is passed to it.
+By default, i.e., if no such method is defined for a given solver, it returns `NullSolverState`.
+"""
+SolverState(::Union{<:AbstractSolver,<:SolverMethod}, args...; kwargs...) = NullSolverState()
