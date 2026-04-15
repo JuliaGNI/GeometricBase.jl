4f2e0600b769ec5b78dc3ae5d9bef8a852c95d98
benedict-96 <benedikt.brantner@icloud.com>
2026-04-15 18:58:48 +0200

    Checking for empty vector.

--- src/utils/norms.jl
+++ src/utils/norms.jl
@@ -1,11 +1,17 @@
 
-function L2norm(x)
+function _L2norm(x)
     mapreduce(xᵢ -> xᵢ * xᵢ, +, x)
 end
 
-function L2norm(x, y)
+function _L2norm(x, y)
     @assert axes(x) == axes(y)
     mapfoldl(z -> (z[1] - z[2])^2, +, zip(x, y))
+end
+
+L2norm(x...) = _L2norm(x...)
+
+function L2norm(x::AbstractVector{T}) where {T}
+    length(x) == 0 ? zero(T) : _L2norm(x)
 end
 
 l2norm(x) = sqrt(L2norm(x))
